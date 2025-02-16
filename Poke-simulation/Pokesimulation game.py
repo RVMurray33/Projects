7a087e2167c362ff5b77e4902e5e5475750ac18e
RVMurray33 <158562697+RVMurray33@users.noreply.github.com>
2025-02-15 21:44:44 -0500

    Update Pokesimulation game.py

--- Poke-simulation/Pokesimulation game.py
+++ Poke-simulation/Pokesimulation game.py
@@ -1,22 +1,55 @@
+from fastapi import FastAPI, HTTPException
 import requests
-from fastapi import FastAPI
+import random
+
 
 app = FastAPI()
 
-@app.get("/pokemon/{name}")
+active_battle = None
+
+class Pokemon:
+    def __init__(self, name, hp, attack, defense, speed, moves, type):
+        self.name = name
+        self.max_hp = hp
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.speed = speed
+        self.moves = moves
+        self.type = type
+    
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp < 0:
+            self.hp = 0
+        
+    def fainted(self):
+        return self.hp <= 0
+
+    def choose_move(self):
+        return random.choice(self.moves)
+
 def get_pokemon(name: str):
     url = f"https:pokeapi.co/api/v2/pokemon/{name.lower()}"
-    requests = requests.get(url)
+    response = requests.get(url)
 
     if response.status_code != 200:
         return {"error": "Pokémon not found"}
 
     data = response.json()
-        return {
-            "name" : data["name"],
-            "id" : data["id"],
-            "hp" : data["hp"],
-            "attack" : data["attack"],
-            "defense" : data["defense"],
-            "sprite" : data["sprite"]
-        }
+
+@app.get("/pokemon/{name}")
+def get_pokemon_api(name: str):
+    pkmn_data = get_pokemon(name)
+    if pkmn_data is None:
+        raise HTTPException(status_code=404, detail="Pokémon not found")
+
+@app.post("/start_battle/")
+def battle(player_pokemon: str, opponent_pokemon: str):
+    global active_battle
+
+    p1_data = get_pokemon(player_pokemon)
+    p2_data = get_pokemon(opponent_pokemon)
+
+@app.post("/attack/")
+def select_move(self, pokemon_data):
