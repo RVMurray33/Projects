4b65ecd6c9ca6ad89a392a2f43ae87476b77dbab
RVMurray33 <158562697+RVMurray33@users.noreply.github.com>
2025-02-17 22:15:11 -0500

    Update battle_routes.py

--- Poke-simulation/battle_routes.py
+++ Poke-simulation/battle_routes.py
@@ -9,3 +9,12 @@ active_battles = {}
 def start_battle(player_pokemon: str, opponent_pokemon: str):
   player_data = get_pokemon(player_pokemon)
   opponent_data = get_pokemon(opponent_pokemon)
+
+  battle_id = random.randint(1000,9999)
+  active_battles[battle_id] = {
+    "player": player_data,
+    "oppponent": opponent_data,
+    "turn": "player"
+  }
+
+  return {"battle_id": battle_id, "message": "Battle started!"}
